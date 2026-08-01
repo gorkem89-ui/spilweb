9ff445176dcf9a27f26984b73c4bdab28f1165fb
gorkem89-ui <spilweb.45@gmail.com>
2026-08-01 17:52:10 +0000

    GitHub Pages router bağlantısı düzeltildi

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -238,7 +238,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior: () => ({ top: 0 })
 });
